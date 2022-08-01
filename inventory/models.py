4b8c4b8bb21122abe5b42d6c7efea2a5d3981c87
Jang-Jaewon <jewon119@gmail.com>
2022-08-02 08:23:53 +0900

    Add: str method 추가 및 superuser 생성

--- inventory/models.py
+++ inventory/models.py
@@ -5,6 +5,9 @@ class Product(models.Model):
     name = models.CharField(max_length=100)
     quantity = models.IntegerField()
 
+    def __str__(self):
+        return self.name
+
 
 class Brand(models.Model):
     name = models.CharField(max_length=100)
