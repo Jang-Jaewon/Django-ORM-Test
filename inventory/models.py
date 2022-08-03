8fc7d6358ea9df47e4405e4cc3437286f435b4a6
Jang-Jaewon <jewon119@gmail.com>
2022-08-04 08:16:06 +0900

    Model Field 유형 수정

--- inventory/models.py
+++ inventory/models.py
@@ -3,10 +3,10 @@ from django.db import models
 
 class Brand(models.Model):
     brand_id = models.BigAutoField(primary_key=True)
-    name = models.CharField(max_length=50)
+    name = models.CharField("Brand Name", max_length=50)
 
     def __str__(self):
-        return self.name
+        return f"Name: {self.name}"
 
 
 class Category(models.Model):
@@ -21,12 +21,14 @@ class Category(models.Model):
 
 class Product(models.Model):
     name = models.CharField("Product Name", max_length=100, default="no-name")
-    age = models.IntegerField()
+    price = models.DecimalField(max_digits=5, decimal_places=2)
+    created_time = models.DateTimeField(auto_now_add=True)
+    updated_time = models.DateTimeField(auto_now=True)
+    url = models.SlugField()
     is_active = models.BooleanField(default=True)
-    category = models.ManyToManyField("Category")
 
     class Meta:
-        ordering = ["age"]
+        ordering = ["-id"]
 
     def __str__(self):
         return f"Product name: {self.name}"
