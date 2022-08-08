740c2bf45c2e2ebfd1252480f2fde1003d40647e
Jang-Jaewon <jewon119@gmail.com>
2022-08-09 08:44:43 +0900

    Add: query 찍어보면, save와 create는 작동하는 방식이 다르다.

--- ex.py
+++ ex.py
@@ -1,15 +1,31 @@
 # Django Shell
 
 from inventory.models import Brand, Category
+from django.db import connection, reset_queries
 
 Brand.objects.all().delete()
 
 Brand.objects.create(brand_id=1, name="Nike")
 
-Brand(brand_id=1, name="Nike2").save()
+Brand(brand_id=1000, name="Nike2").save()
 
 Category.objects.all().delete()
 
 Category.objects.create(name="Trainers", slug="trainers", is_active=True)
 
 Category(id=3, name="Trainers100", slug="trainers2", is_active=True).save()
+
+
+reset_queries()
+
+Brand.objects.create(brand_id=1, name="Nike")
+connection.queries
+reset_queries()
+
+Brand(brand_id=1000, name="Nike2").save()
+connection.queries
+reset_queries()
+
+Brand(brand_id=1000, name="Nike1000").save()
+connection.queries
+reset_queries()
