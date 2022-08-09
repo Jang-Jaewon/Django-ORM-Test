61fe0a0326e32958045972b5670464e37f6c9ac1
Jang-Jaewon <jewon119@gmail.com>
2022-08-09 09:01:41 +0900

    Add: SQL로 Object 생성하기

--- ex.py
+++ ex.py
@@ -1,31 +1,14 @@
 # Django Shell
 
-from inventory.models import Brand, Category
 from django.db import connection, reset_queries
+from inventory.models import Brand
 
 Brand.objects.all().delete()
 
-Brand.objects.create(brand_id=1, name="Nike")
-
-Brand(brand_id=1000, name="Nike2").save()
-
-Category.objects.all().delete()
-
-Category.objects.create(name="Trainers", slug="trainers", is_active=True)
-
-Category(id=3, name="Trainers100", slug="trainers2", is_active=True).save()
+cursor = connection.cursor()
+cursor.execute("INSERT INTO inventory_brand (brand_id, name) VALUES (%s, %s)", ['1', 'nike'])
 
 
 reset_queries()
-
-Brand.objects.create(brand_id=1, name="Nike")
+Brand.objects.create(brand_id=100, name="nike100")
 connection.queries
-reset_queries()
-
-Brand(brand_id=1000, name="Nike2").save()
-connection.queries
-reset_queries()
-
-Brand(brand_id=1000, name="Nike1000").save()
-connection.queries
-reset_queries()
