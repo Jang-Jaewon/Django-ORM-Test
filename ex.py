b9c07d191a77fb21eced6c316b62e5eb014b2674
Jang-Jaewon <jewon119@gmail.com>
2022-08-16 08:41:52 +0900

    Fixed: Modeling 변경 및 마이그레이션

--- ex.py
+++ ex.py
@@ -1,14 +1,1 @@
 # Django Shell
-
-from django.db import connection, reset_queries
-from inventory.models import Brand
-
-Brand.objects.all().delete()
-
-cursor = connection.cursor()
-cursor.execute("INSERT INTO inventory_brand (brand_id, name) VALUES (%s, %s)", ['1', 'nike'])
-
-
-reset_queries()
-Brand.objects.create(brand_id=100, name="nike100")
-connection.queries
