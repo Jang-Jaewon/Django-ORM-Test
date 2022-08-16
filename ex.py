f5c4d95f573cefd8d97a2a8d5887b384887d3adb
Jang-Jaewon <jewon119@gmail.com>
2022-08-16 09:01:02 +0900

    Create Test In Shell

--- ex.py
+++ ex.py
@@ -1,1 +1,10 @@
 # Django Shell
+from inventory.models import Product, ProductInventory, ProductType, Brand
+
+Brand.objects.create(brand_id=1, name="nike")
+
+Product(web_id="1234", slug="nike-shoe-1", name="nike-shoe-1", description="ex2", is_active=True).save()
+
+ProductType.objects.create(name="shoe")
+
+ProductInventory.objects.create(sku="123", upc="123", product_type_id=1, product_id=1, brand_id=1, retail_price="10.00", store_price="10.00", sale_price="10.00", weight="100")
