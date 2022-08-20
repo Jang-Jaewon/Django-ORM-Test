fee03d0a9b01a0c75d051a034626c853ee7fcd0d
Jang-Jaewon <jewon119@gmail.com>
2022-08-20 16:18:28 +0900

    Add: SQL로 삽입하는 파일부터 별도 파일로 기록 남기기

---
+++ 001_insert_sql.py
@@ -0,0 +1,24 @@
+# Django Shell
+import datetime
+from inventory.models import Product, ProductInventory, ProductType, Brand
+from django.db import connection
+
+Brand.objects.create(brand_id=1, name="nike")
+ProductInventory.objects.all().delete()
+Brand.objects.all().delete()
+Product.objects.all().delete()
+ProductType.objects.all().delete()
+
+Brand.objects.create(brand_id=1, name="nike")
+Product(web_id="1234", slug="nike-shoe-1", name="nike-shoe-1", description="ex2", is_active=True).save()
+
+ProductType.objects.create(name="shoe")
+
+ProductInventory.objects.create(sku="123", upc="123", product_type_id=1, product_id=1, brand_id=1, retail_price="10.00", store_price="10.00", sale_price="10.00", weight="100")
+datetime_object = datetime.datetime.now()
+datetime_object
+print(datetime_object)
+
+cursor = connection.cursor()
+cursor.execute("INSERT INTO inventory_productinventory(sku,upc,product_type_id,product_id,brand_id,is_active,is_default,retail_price,store_price,sale_price,is_on_sale,is_digital,weight,created_at,updated_at) VALUES (%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)",['123','123',1,1,1,True,True,'10.00','10.00','10.00',True,True,'100','2022-08-17 23:47:05.813475','2022-08-17 23:47:05.813475'])
+ProductInventory.objects.values()
